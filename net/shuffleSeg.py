7090b3facc1789bfa9aa9a7201c50d7b2eec32f6
范仲悦 <fanzhongyue@bytedance.com>
2018-09-05 10:29:19 +0800

    add shuffleSegNet

--- net/shuffleSeg.py
+++ net/shuffleSeg.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import slim
+
 class BasicUnitA(nn.Module):
     def __init__(self, in_c, out_c, group, stride=2, dilation=1):
         super(BasicUnitA, self).__init__()
@@ -20,10 +21,8 @@ class BasicUnitA(nn.Module):
         self.g_conv4 = slim.conv_bn(bottleneck, out_c, 1, stride=1, pad=0, group=group)
 
         self.avg_pool1 = nn.AvgPool2d(3, 2, 1)
-        print bottleneck
 
     def forward(self, x):
-        print x.size()
         out = self.g_conv1(x)
         out = self.shuffle2(out)
         out = self.dw_conv3(out)
@@ -51,14 +50,64 @@ class BasicUnitB(nn.Module):
         out = self.g_conv4(out)
         return F.relu(out + x)
 
+class LayoutNet(nn.Module):
+    def __init__(self, num_classes):
+        super(LayoutNet, self).__init__()
+        group = 2
+        self.stage1 = nn.Sequential(
+            slim.conv_bn_relu(c_in=3, c_out=24, k_size=3, stride=2, pad=0),
+            nn.MaxPool2d(2, 2) 
+            )
+        self.stage2 = nn.Sequential(BasicUnitA(24, 240, group),
+            BasicUnitB(240, 240, group),
+            BasicUnitB(240, 240, group),
+            )
+        self.stage3 = nn.Sequential(BasicUnitA(240, 480, group),
+            BasicUnitB(480, 480, group),
+            BasicUnitB(480, 480, group),
+            BasicUnitB(480, 480, group),
+            BasicUnitB(480, 480, group),
+            BasicUnitB(480, 480, group),
+            BasicUnitB(480, 480, group),
+            )
+        self.stage4 = nn.Sequential(BasicUnitA(480, 960, group),
+            BasicUnitB(960, 960, group),
+            BasicUnitB(960, 960, group),
+            )
+
+        self.classifier1 = nn.Conv2d(240, num_classes, 1, 1)
+        self.classifier2 = nn.Conv2d(480, num_classes, 1, 1)
+        self.classifier3 = nn.Conv2d(960, num_classes, 1, 1)
+        self.num_classes = num_classes
+
+    def forward(self, imgs, labels):
+        n, h, w = labels.shape
+
+        out = self.stage1(imgs)
+        out = self.stage2(out)
+        preds1 = self.classifier1(out)
+        preds1 = F.upsample_bilinear(preds1, size=(h, w))
+
+        out = self.stage3(out)
+        preds2 = self.classifier2(out)
+        preds2 = F.upsample_bilinear(preds2, size=(h, w))
+
+        out = self.stage4(out)
+        preds3 = self.classifier3(out)
+        preds3 = F.upsample_bilinear(preds3, size=(h, w))
+
+        #print preds.shape
+        preds = preds1 + preds2 + preds3
+        loss = F.nll_loss(F.log_softmax(preds), labels)
+        return preds, loss, torch.stack([preds1, preds2, preds3])
+
+
 if __name__ == '__main__':
     from torch.autograd import Variable
-    stage1 = nn.Sequential(BasicUnitA(24, 200, 2),
-            BasicUnitB(200, 200, 2),
-            BasicUnitB(200, 200, 2),
-            BasicUnitB(200, 200, 2),
+    stage1 = nn.Sequential(BasicUnitA(24, 240, 4),
+            BasicUnitB(240, 240, 4),
+            BasicUnitB(240, 240, 4),
             )
     x = Variable(torch.rand(2, 24, 64, 64), volatile=False)
     out = stage1(x)
-    print out.size()
 
